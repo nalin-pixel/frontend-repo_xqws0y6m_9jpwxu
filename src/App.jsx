00494264aa276fec6e33540746082b124fd3e578
Flames <vibe-coder@flames.com>
2025-11-06 10:00:59 +0000

    Update project prj_xqws0y6m - 2025-11-06T10:00:58.880Z

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
-import Experience from './components/Experience';
-import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,8 +10,6 @@ function App() {
       <Hero />
       <About />
       <Skills />
-      <Experience />
-      <Gallery />
       <Footer />
     </div>
   );
